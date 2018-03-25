09d8e075c1b94f30c92eb9a6ba6f19f1b70ad5fe
zc3212521 <827058891@qq.com>
2018-03-25 22:23:34 +0800

    添加编辑器

--- client/views/topicList/index.jsx
+++ client/views/topicList/index.jsx
@@ -1,5 +1,6 @@
 import React  from 'react'
 import PropTypes from 'prop-types'
+import {Editor, EditorState} from 'draft-js'
 
 import {
     observer,
@@ -7,12 +8,29 @@ import {
 } from 'mobx-react'
 
 @inject('appState') @observer
-export default class TopicList extends React.Component {
+export default class MyEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {editorState: EditorState.createEmpty()};
+        this.onChange = (editorState) => this.setState({editorState});
+    }
     render() {
-        return <div>{this.props.appState.msg}</div>
+        return (
+            <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        );
     }
 }
 
-TopicList.propTypes = {
-    appState: PropTypes.object
-}
+
+
+// class TopicList extends React.Component {
+//     render() {
+//         return <div>{this.props.appState.msg}</div>
+//     }
+// }
+
+
+//
+// TopicList.propTypes = {
+//     appState: PropTypes.object
+// }
